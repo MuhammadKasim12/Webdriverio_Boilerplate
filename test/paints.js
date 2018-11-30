607df033a8a237895613c713a220f8e488bff2e2
--replace-all <--replace-all>
2018-11-29 19:50:53 -0800

    adding webdriver io boiler plate project solutions

---
+++ test/paints.js
@@ -0,0 +1,16 @@
+var paints = [{ color: 'red'}, { color: 'blue'}, {color: 'yellow'}];
+
+
+
+function pluck (paints, property) {
+    console.log(paint[property]);
+  paints.map(function(paint,property){
+    console.log(paint[property]);
+    return paint[property];   
+  });
+}
+  
+var colors = pluck[paints,'color'];
+
+colors
+
